85231be1ac628a7bea705f02471ceaacb29d20fa
AlifaNovelia <69961570+AlifaNovelia@users.noreply.github.com>
2020-08-20 15:48:03 +0700

    Add files via upload

---
+++ object&class.php
@@ -0,0 +1,13 @@
+<?php
+class latihan {
+   //... isi dari class latihan
+   }
+ 
+$a = new latihan();
+echo var_dump($a)."<br>";
+$b = new latihan ();
+echo var_dump($b)."<br>";
+$c = new latihan ();
+echo var_dump($c)."<br>";
+
+?>
